8a7bd861b626d63f8c35627d4926ae0b4d2f7925
ABIODUN <ayobamiabiodun90@gmail.com>
2022-09-23 17:44:44 +0100

    Operators

---
+++ myjavascript.js
@@ -0,0 +1,21 @@
+let multiple1 = 3
+multiple1 *= 2
+
+document.getElementById('ans').innerHTML = "<code> let multiple1=3 </code> <br> <code> multiple1 *=2 </code> <br> <code>" + multiple1;
+// window.alert(multiple1)
+
+
+let exponent = 10
+exponent **= 10
+
+document.getElementById('expo').innerHTML = "<code> let exponent=10</code><br> <code> exponent **=10 </code><br>" + exponent;
+
+let modu = 20
+modu %= 3
+
+document.getElementById('modulous').innerHTML = "<code>let modu = 20 </code><br> <code>modu % =3</code><br>" + modu;
+
+let divnumber = 30
+divnumber /= 5
+
+document.getElementById('diviop').innerHTML = "<code>let divnumber = 30</code><br> <code>divnumber /= 5 </code><br>" + divnumber;
